9c15f5e9f1fb119ec4146685d1c98996a79f6cf2
AlexanderPuentedelaVega <alexander.puentedela@tecsup.edu.pe>
2024-09-30 17:27:15 -0500

    Initial Commit

---
+++ PuentedelaVegaColeccionDeJuegos3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PuentedelaVegaColeccionDeJuegos3
+//
+//  Created by Alexander Puente de la Vega on 30/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
